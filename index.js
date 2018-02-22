df89fa593efb06a2be888fc7ff188269a0230f5b
wendy yu <yu.wendyw@gmail.com>
2018-02-22 08:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,7 @@ function dec(n) {
   return n-1
 }
 function makeInt(n) {
-  return parseInt(n)
-}
-function makeInt(n) {
-  parseInt(n, 10)
+  return parseInt(n, 10)
 }
 function preserveDecimal(n) {
   parseFloat(n)
